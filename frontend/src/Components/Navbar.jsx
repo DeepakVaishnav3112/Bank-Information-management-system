0a6008a9f6510893891e15681e3674d73cb9b754
Deepak Vaishnav <deepakvaishnav7740@gmail.com>
2024-10-20 01:57:24 +0530

    fixed logout related issues

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const logout = () => {
     localStorage.removeItem('token')
     delete axios.defaults.headers.common['Authorization'];
-    navigate('/login')
+    navigate("/user/login")
   }
 
   return (
